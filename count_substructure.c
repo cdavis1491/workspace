01c35d1e970278849a35281380541cc01f035baa
Christina Davis <cdavis.loves.science@gmail.com>
2016-04-19 11:17:20 -0500

    TEST

--- count_substructure.c
+++ count_substructure.c
@@ -26,14 +26,46 @@ int main()
 		//open newhalos_xxx.txt
 		char filename[50];
 		sprintf(filename, "/fs0/illustris_snapshots/HPC_testfiles/newhalos_%d.txt", snap);
+		FILE *fp_in 
 		fp_in = fopen(filename, "r");
+		
 		//check to make sure file opened
 		if ( fp_in == NULL ) {
       		printf("input_file.txt not opened, exiting...\n");
       		exit(0);
     	}
+    	
+    	int Nhalos = 0;  
+    	//the number of lines in the file will be equal to the number of halos. 
+    	//this tells me how long my NEWdeepID_array and hostHalo arrays need to be
+		int ch;
+		while(!feof(fp_in))
+		{
+			ch = fgetc(fp_in);
+			if(ch == '\n')
+			{
+				Nhalos++;
+			}
+		}
+    	
+    	printf("number of halos in the file was %d\n", Nhalos);
+    	//allocate/initialize arrays for NEWdeepID 
+    	//NEWdeepID gives the manodeep assigned haloID (int)
+    	//int * NEWdeepID_array = malloc(Nhalos * sizeof(int));
+    	
+    	//allocate/initialize arrays for hostHalo 
+    	//hostHalo says which FOF name the current halo belongs to 
+    	//if 0 then it IS the FOF 
+    	//int * hostHalo = malloc(Nhalos * sizeof(int));
+
+    	//first line is header so actual number of elements is 1 less than
+    	//the number of lines.  
+    	//Nhalos = Nhalos - 1 
+		
 		//unpack first column = NEWdeepID 
 		//unpack second column = hostHalo 
+		
+
 		//print 'starting snap blah'
 		//create substructure array
 		//call substructure function to fill array
