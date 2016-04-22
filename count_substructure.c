f8171c5ad6cac6b1919ca56e3dd1c88631c930cc
Christina Davis <cdavis.loves.science@gmail.com>
2016-04-21 19:00:05 -0500

    DEBUG

--- count_substructure.c
+++ count_substructure.c
@@ -160,14 +160,15 @@ void count_subs(long int * Nhalos, long long int * NEWdeepID_array, long long in
 				continue;
 			}
 		}
+		printf("iteration : %d\n", halo);
 		//now we've gone through the whole list so we have counted numsubs for this halo
 		//fill substructure_array with numsubs 
 		substructure_array[halo] = numsubs; 
-		if (halo%1000 == 0){
-			printf("iteration : %d\n", halo);
-			printf("HaloID : %lli\n", NEWdeepID_array[halo]);
-			printf("Numsubs : %lli\n", numsubs);
-		}
+		// if (halo%1000 == 0){
+		// 	printf("iteration : %d\n", halo);
+		// 	printf("HaloID : %lli\n", NEWdeepID_array[halo]);
+		// 	printf("Numsubs : %lli\n", numsubs);
+		// }
 	}
 	return;
 }
