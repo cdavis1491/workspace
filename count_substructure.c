2a22236587ccb493c9d4dc90448394c346c784f7
Christina Davis <cdavis.loves.science@gmail.com>
2016-04-21 19:16:02 -0500

    DEBUG

--- count_substructure.c
+++ count_substructure.c
@@ -92,9 +92,6 @@ int main()
 		}
 		*/
 
-		printf("the 37700th element of HaloID : %lli\n", NEWdeepID_array[37700]); 
-		printf("the 37701th element of HaloID : %lli\n", NEWdeepID_array[37701]);
-
 		//print 'starting snap blah'
 		printf("Starting substructure on snapshot : %d \n", snap);
 
