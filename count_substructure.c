57b37d018accd454c644efcd6bf42ff801fb1764
Christina Davis <cdavis.loves.science@gmail.com>
2016-04-21 19:23:45 -0500

    DEBUG

--- count_substructure.c
+++ count_substructure.c
@@ -108,7 +108,7 @@ int main()
 		sprintf(outfilename, "/fs0/illustris_snapshots/HPC_testfiles/substructure_output_%d.txt", snap);
 		FILE *fp_out;
 		fp_out = fopen(outfilename, "w");
-		printf("The first element of NEWdeepID_array : %lli", NEWdeepID_array[0])
+		printf("The first element of NEWdeepID_array : %lli", NEWdeepID_array[0]);
 		for (k = 0; k < Nhalos; k++){
 			fprintf(fp_out, "%d , %d \n", NEWdeepID_array[k], substructure_array[k]);
 		}
